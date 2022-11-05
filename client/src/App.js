acaae7cd1cfd3a42a0b5e171ff7c1bd96c2d36ea
Charles Duran <62945929+4Charlie4@users.noreply.github.com>
2022-11-04 21:34:13 -0400

    mutations added to pages

--- client/src/App.js
+++ client/src/App.js
@@ -1,21 +1,39 @@
-import React from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import SearchBooks from './pages/SearchBooks';
-import SavedBooks from './pages/SavedBooks';
-import Navbar from './components/Navbar';
+import React from "react";
+import {
+  ApolloProvider,
+  ApolloClient,
+  InMemoryCache,
+  createHttpLink,
+} from "@apollo/client";
+
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import SearchBooks from "./pages/SearchBooks";
+import SavedBooks from "./pages/SavedBooks";
+import Navbar from "./components/Navbar";
+
+const httpLink = createHttpLink({
+  uri: "/graphql",
+});
+
+const client = new ApolloClient({
+  link: httpLink,
+  cache: new InMemoryCache(),
+});
 
 function App() {
   return (
-    <Router>
-      <>
-        <Navbar />
-        <Routes>
-          <Route exact path='/' element={<SearchBooks/>} />
-          <Route exact path='/saved' element={<SavedBooks/>} />
-          <Route render={() => <h1 className='display-2'>Wrong page!</h1>} />
-        </Routes>
-      </>
-    </Router>
+    <ApolloProvider client={client}>
+      <Router>
+        <>
+          <Navbar />
+          <Routes>
+            <Route exact path="/" element={<SearchBooks />} />
+            <Route exact path="/saved" element={<SavedBooks />} />
+            <Route render={() => <h1 className="display-2">Wrong page!</h1>} />
+          </Routes>
+        </>
+      </Router>
+    </ApolloProvider>
   );
 }
 
